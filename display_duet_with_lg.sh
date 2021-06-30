c3a0b34b787b89481707544662f526d010b5a836
kidpeterpan <zealotkid@gmail.com>
2021-06-30 22:27:37 +0700

    add display setting cmd

---
+++ display_duet_with_lg.sh
@@ -0,0 +1,7 @@
+# duet
+xrandr --auto --output DSI-1 --mode 1200x1920 --right-of DP-2 --rotate right
+# lg
+gtf 2560 1080 60
+xrandr --newmode "2560x1080_60.00"  230.76  2560 2728 3000 3440  1080 1081 1084 1118  -HSync +Vsync
+xrandr --addmode DP-2 "2560x1080_60.00"
+xrandr --output DP-2 --auto --mode "2560x1080_60.00" --left-of DSI-1
---
+++ display_only_duet.sh
@@ -0,0 +1,3 @@
+# xrandr -q for query and see connected display name then
+xrandr --auto --output DSI-1 --mode 1200x1920 --rotate right
+xrandr --output DP-2 --off
