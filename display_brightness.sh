da47cb9ab3b94012e511ab2aa616a8ba1cdceeb9
kidpeterpan <zealotkid@gmail.com>
2021-07-04 21:44:35 +0700

    update cmd

--- display_brightness.sh
+++ display_brightness.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Enter the screen ID"
+echo "Enter the screen ID (hint DSI-1 or DP-2)"
 read first
 echo "Enter value of brightness (0 to 1)"
 read second
---
+++ keyboard_language_only_en.sh
@@ -0,0 +1,2 @@
+# add keyboard language
+setxkbmap -layout us
