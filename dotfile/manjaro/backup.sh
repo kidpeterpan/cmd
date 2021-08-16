245da3c7b2b7797d96b577546802d7568644e86b
kidpeterpan <zealotkid711@gmail.com>
2021-08-16 21:45:46 +0700

    back up manjaro dotfile

--- dotfile/manjaro/backup.sh
+++ dotfile/manjaro/backup.sh
@@ -1,2 +1,3 @@
-cp ~/.config/i3/config ./i3/config
+cp ~/.i3/config ./i3/config
+cp ~/.i3status-rs/config.toml ./i3status-rs/config.toml
 cp ~/.zshrc ./zshrc
--- dotfile/manjaro/restore.sh
+++ dotfile/manjaro/restore.sh
@@ -1,2 +1,4 @@
 cp ./zshrc ~/.zshrc
-cp ./i3/config ~/.config/i3/config
+# this is for manjaro i3 version
+cp ./i3/config ~/.i3/config
+cp ./i3status-rs/config.toml ~/.i3status-rs/config.toml
