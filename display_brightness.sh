817333d5cf49e3116caf38e1a44d74531e070d11
kidpeterpan <zealotkid711@gmail.com>
2021-08-22 00:53:33 +0700

    update brightness

--- display_brightness.sh
+++ display_brightness.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo "Enter the screen ID (hint DSI-1 or DP-2)"
+echo "Enter the screen ID"
+echo "DP-4 for Legion 5"
+echo "DSI-1 for Duet 3i"
 read first
 echo "Enter value of brightness (0 to 1)"
 read second
